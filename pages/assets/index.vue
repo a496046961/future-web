ddd7bed645cf39472d06f051b4249fa6b5abff79
c <23@g>
2025-04-27 23:40:46 +0700

    init project

---
+++ pages/assets/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<p>资产</p>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/trade/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<p> 交易相关 </p>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
